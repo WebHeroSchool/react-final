ae395cc0320a39d9d07cb5a44b562d37eecfe578
Татьяна Соснина <tatyana-sosnina@mail.ru>
2021-02-17 18:28:56 +0700

    final

---
+++ src/components/About/About.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styles from './About.module.css';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { Octokit } from '@octokit/rest';
+import Card from '@material-ui/core/Card';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Repositories from '../Repositories/Repositories';
+
+const octokit = new Octokit();
+
+class About extends React.Component {
+    state = {
+        isLoading: true,
+        userInfo: [],
+        isError: false,
+        errorMessage: ''
+    }
+
+    receivedUserInfo() {
+        octokit.users.getByUsername({
+            username: 'Tatyana888-star'
+          })
+          .then(({ data }) => {
+            this.setState({
+                userInfo: data,
+                isLoading: false
+            });
+        })
+            .catch(err => {
+             this.setState({
+                 isLoading: false,
+                 isError: true,
+                 errorMessage: err
+             });
+        });
+    }
+
+    componentDidMount() {
+        this.receivedUserInfo()
+    }
+
+    render() {
+        const {
+           isLoading,
+           userInfo,
+           isError,
+           errorMessage
+         } = this.state;
+         const divStyle={
+             color: 'red',
+             minHeight: '100px',
+             display: 'flex',
+             alignItems: 'center'
+        };
+         const errMsg = 'Ошибка. Не удалось получить данные о пользователе: ' + errorMessage;
+
+         return (
+              <div className = {styles.wrap}>
+                 <Router>
+                 { isLoading ? <LinearProgress color = "secondary" /> :
+                     <Card>
+                         {isError ?  <div style={divStyle}>{errMsg}</div> :
+                              <div className={styles.info}>
+                                 <img src ={userInfo.avatar_url} className={styles.avatar} alt='avatar'/>
+                                 <div className={styles.infoBlock}>
+                                    <p className={styles.name}>{userInfo.name ? userInfo.name : userInfo.login}</p>
+                                    <p className={styles.bio}>{userInfo.bio ? userInfo.bio : 'Описание профиля отсутствует.' }
+                                    </p>
+                                    <a href='/contacts' style={{textDecoration: 'none'}}>
+                                       <div className={styles.contactsButton}>Watch contacts</div>
+                                    </a>
+                                 </div>
+                              </div>
+                            }
+                     </Card>
+                  }
+                  <Repositories />
+                  </Router>
+              </div>
+
+        );
+      }
+  }
+
+  export default About;
---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import styles from './InputItem.module.css';
+
+class InputItem extends React.Component {
+  state = {
+    inputValue: '',
+    isError: false,
+    textOfError: ''
+  };
+
+  inputValueChange = event => this.setState({ inputValue: event.target.value });
+
+  onSubmit= (event) => {
+      event.preventDefault();
+
+      const {onClickAdd, items} = this.props;
+        const {inputValue} = this.state;
+
+        let error = false;
+        items.forEach(item=> {
+          if(item.value.toLowerCase() === inputValue.toLowerCase()) {
+              error = true
+          }
+        });
+         if(inputValue ==='' || error) {
+            this.setState({
+              isError: true,
+              textOfError: error ? 'This case is already  in the list, enter another task, please!': 'Please, fullfill the field!',
+            });
+          }
+         else {
+            this.setState({
+                inputValue: '',
+                isError: false,
+                textOfError: ''
+            });
+            onClickAdd(inputValue);
+          }
+      };
+
+
+  render() {
+    const { isError, textOfError} = this.state
+
+    return (
+       <form
+           className ={styles.itemInput}
+           onSubmit={this.onSubmit}>
+           <TextField
+                   error = {isError}
+                   id='standard-full-width'
+                   label=''
+                   style={{ margin: 0}}
+                   placeholder= 'What needs to be done?'
+                   helperText = {textOfError}
+                   fullWidth
+                   margin='normal'
+                   InputLabelProps={{
+                   shrink: true,}}
+                   value={this.state.inputValue}
+                   onChange ={ this.inputValueChange }
+             />
+           <button
+               className = { styles.button}>
+               ADD
+           </button>
+
+           </form>
+           );
+     }
+   };
+
+export default InputItem;
